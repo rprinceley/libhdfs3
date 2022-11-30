ff60ca5e9a880e1dca10e6993ef061dc55f694b9
cyjseagull <yujiechen_hust@163.com>
2022-12-01 00:48:18 +0800

    add InstallConfig to libhdfs3 && support WITH_HUNTER_PKG (#1)
    * add InstallConfig to libhdfs3
    
    * support WITH_HUNTER_PKG

---
+++ CMake/FindLibXml2.cmake
@@ -0,0 +1,23 @@
+# - Find libxml2
+# Find the native LIBXML2 includes and library
+#
+#  LIBXML2_INCLUDE_DIRS - where to find libxml2/libxml, etc.
+#  LIBXML2_LIBRARIES    - List of libraries when using libxml2.
+#  LIBXML2_FOUND        - True if libxml2 found.
+
+IF (LIBXML2_INCLUDE_DIRS)
+  # Already in cache, be silent
+  SET(LIBUUID_FIND_QUIETLY TRUE)
+ENDIF (LIBXML2_INCLUDE_DIRS)
+
+FIND_PATH(LIBXML2_INCLUDE_DIRS libxml2/libxml)
+
+SET(LIBXML2_NAMES libxml2)
+FIND_LIBRARY(LIBXML2_LIBRARIES NAMES ${LIBXML2_NAMES})
+
+# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBXML2 DEFAULT_MSG LIBXML2_LIBRARIES LIBXML2_INCLUDE_DIRS)
+
+MARK_AS_ADVANCED(LIBXML2_LIBRARIES LIBXML2_INCLUDE_DIRS)
